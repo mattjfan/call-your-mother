edfc965c081c37dad4e3332723bd82f736c67e04
Jesse Wu <jessewu2016@gmail.com>
2019-12-01 17:49:40 -0500

    notifications schedule function

--- app/src/main/java/com/example/callyourmother/notification/Notifications.kt
+++ app/src/main/java/com/example/callyourmother/notification/Notifications.kt
@@ -1,8 +1,8 @@
 package com.example.callyourmother.notification
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import com.example.callyourmother.fragments.UserFragment
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
@@ -10,16 +10,15 @@ import android.os.Build.VERSION_CODES
 import androidx.core.app.NotificationCompat
 import android.os.Build.VERSION
 import androidx.core.app.NotificationManagerCompat
+import java.util.*
 
 
 class Notifications {
 
-    fun notify(context: Context,title: String,description: String, CHANNEL_ID: String, iconID: Int,
+    public fun notify(context: Context,title: String,description: String, CHANNEL_ID: String, iconID: Int,
                notificationID: Int) {
-        val intent = Intent(context, UserFragment::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent : PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+        val intent = Intent(context, AlarmReceiver::class.java)
+        val pendingIntent : PendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
         var builder = NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(iconID)
                 .setContentTitle(title)
@@ -33,7 +32,7 @@ class Notifications {
         }
     }
 
-    private fun createNotificationChannel(context: Context, CHANNEL_ID: String, CHANNEL_NAME: String, CHANNEL_DESCRIPTION: String) {
+    public fun createNotificationChannel(context: Context, CHANNEL_ID: String, CHANNEL_NAME: String, CHANNEL_DESCRIPTION: String) {
         if(VERSION.SDK_INT >= VERSION_CODES.O) {
             val name = CHANNEL_NAME
             val descriptionText = CHANNEL_DESCRIPTION
@@ -44,6 +43,49 @@ class Notifications {
             val NotificationManager: NotificationManager =
                     context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             NotificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    public fun setScheduledNotification(context : Context, schedule: Int, lastCalled: Calendar,
+                                        userID: Int, title: String, body: String) {
+        val manager : AlarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        val intent : Intent = Intent(context, AlarmReceiver::class.java)
+        intent.putExtra("notificationBody", body)
+        intent.putExtra("notificationTitle", title)
+        intent.putExtra("userID", userID)
+
+        val pendingIntent : PendingIntent = PendingIntent.getBroadcast(context,0,intent,0)
+
+        //Today's date
+        val today : Calendar = Calendar.getInstance()
+        today.timeInMillis = System.currentTimeMillis()
+
+        //Scheduling the next call
+        val nextCall : Calendar = lastCalled
+
+        when(schedule) {
+            1 -> nextCall.add(Calendar.DAY_OF_YEAR, 1)
+            7 -> nextCall.add(Calendar.DAY_OF_YEAR, 7)
+            14 -> nextCall.add(Calendar.DAY_OF_YEAR, 14)
+            30 -> nextCall.add(Calendar.DAY_OF_YEAR, 30)
+            365 -> nextCall.add(Calendar.DAY_OF_YEAR, 365)
+        }
+
+        //Determine the interval in which alarms should be made
+        var interval : Long = 86400000
+        when(schedule) {
+            1 -> interval = interval
+            7 -> interval = interval*7
+            14 -> interval = interval*14
+            30 -> interval = interval*30
+            365 -> interval = interval*365
+        }
+
+        if(nextCall.before(today)) {
+            manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, today.timeInMillis, interval, pendingIntent)
+        }else {
+            manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, nextCall.timeInMillis, interval, pendingIntent)
         }
     }
 }
